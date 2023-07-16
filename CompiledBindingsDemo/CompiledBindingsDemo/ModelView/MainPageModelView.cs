b9bd35538f6380788c9113e3467c0e670c774275
AlbertoJTD <albertojtd@outlook.com>
2023-07-16 14:13:23 -0600

    Add MVVM pattern

---
+++ CompiledBindingsDemo/CompiledBindingsDemo/ModelView/MainPageModelView.cs
@@ -0,0 +1,45 @@
+﻿using CompiledBindingsDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CompiledBindingsDemo.ModelView
+{
+	public class MainPageModelView
+	{
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public MainPageModelView()
+        {
+            Customers = new ObservableCollection<Customer>
+            {
+                new Customer
+                {
+                    Name = "John",
+                    Age = 32
+                },
+				new Customer
+				{
+					Name = "Jonny",
+					Age = 40
+				},
+				new Customer
+				{
+					Name = "Jax",
+					Age = 45
+				},
+				new Customer
+				{
+					Name = "Ermac",
+					Age = 87
+				},
+				new Customer
+				{
+					Name = "Sub-zero",
+					Age = 50
+				},
+			};
+        }
+    }
+}
